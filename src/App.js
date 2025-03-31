73321be6152a489406d6e6e95889f4f3b8d6f5aa
Nilesh Sambar <nileshsambar7@gmail.com>
2025-03-31 22:26:47 +0530

    Updated project with new changes

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
      
           
       <Routes>
-        <Route path="/meeting/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/signup/" element={<SignUp />} />
         <Route path="/signin/" element={<SignIn />} />
         <Route path="/preferences" element={<UserPreferences />} />
